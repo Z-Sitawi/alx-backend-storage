a90acdcde0127ce94c8a1680884ad3479af6053e
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-08-03 18:15:55 +0100

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+A function that changes all topics of a school document
+based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Changes all topics of a school document based on the name
+    :param mongo_collection: pymongo collection object
+    :param name: (string) the school name to update
+    :param topics:  (list of strings) topics approached in the school
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
