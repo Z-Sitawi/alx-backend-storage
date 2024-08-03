696f3ace7a1b15af2c48d16ced4763d757d1719c
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-08-03 18:11:49 +0100

    task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""  A function that inserts a new document
+in a collection based on kwargs """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a collection based on kwargs
+    :param mongo_collection: a pymongo collection object
+    :param kwargs: document to be inserted
+    :return: the new _id
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
