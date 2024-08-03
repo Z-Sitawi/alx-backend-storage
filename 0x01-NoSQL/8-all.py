a6bb78083ce79c4caf255000cf86c33d13287a52
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-08-03 18:02:19 +0100

    task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" A function that lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """
+        lists all documents in a collection
+    :param mongo_collection: is a pymongo collection object
+    :return: A list of all documents
+            or an empty list if no document in the collection
+    """
+    return mongo_collection.find()
